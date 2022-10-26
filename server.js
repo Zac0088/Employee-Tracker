e905e1ea9d39a38d345af794991a4caebb8dbc69
Zac Smart <zac_chaz@outlook.com>
2022-10-26 18:05:27 +1100

    initial question prompt

--- server.js
+++ server.js
@@ -1,4 +1,58 @@
 const inquirer = require("inquirer");
 const db = require('./db/connect')
-const table = require('console.table');
+require('console.table');
 
+db.connect((err) => {
+    if (err) throw err;
+    return promptUser();
+})
+
+const promptUser = () => {
+    inquirer.prompt({
+        type: 'list',
+        message: 'Please select an option',
+        name: 'options',
+        choices: [
+            'View departments',
+            'View Roles',
+            'View Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update Role'
+
+        ]
+    })
+        .then((answer) => {
+            switch (answer.action) {
+                case "Add an Employee":
+                    addEmployee();
+                    break;
+
+                case "Add a Department":
+                    addDepartment();
+                    break;
+
+                case "Add a Role":
+                    addRole();
+                    break;
+
+                case "view Employees":
+                    viewEmployees();
+                    break;
+
+                case "View Departments":
+                    viewDepartment();
+                    break;
+
+                case "View Roles":
+                    viewRoles();
+                    break;
+
+                case "Update Role":
+                    updateEmployeeRole();
+                    break;
+                }
+            })
+        }
+        
